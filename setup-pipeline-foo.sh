5da0f14211d3850978587ec7900cccf14c40c49f
Ingo Jobling <ingo.jobling@gmail.com>
2022-06-20 20:33:30 -0400

    config-pipeline-foo

--- setup-pipeline-foo.sh
+++ setup-pipeline-foo.sh
@@ -3,6 +3,6 @@ set -eux
 
 # This script automatically sets up the self-updating demo pipeline so you can begin experimenting
 fly --target=concourse login --concourse-url=http://localhost:8080 --username=test --password=test
-fly --target=concourse set-pipeline --pipeline=pipeline-foo --config=pipeline-foo.yml --load-vars-from params-foo.yml 
-fly --target=concourse unpause-pipeline --pipeline=pipeline-foo
+fly --target=concourse set-pipeline --pipeline=config-pipeline-foo --config=config-pipeline-foo.yml --load-vars-from params-foo.yml 
+fly --target=concourse unpause-pipeline --pipeline=config-pipeline-foo
 
