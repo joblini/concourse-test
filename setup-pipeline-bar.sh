c2abd7df12037e8adaf6a47a9ca95ed6845bbe29
Ingo Jobling <ingo.jobling@gmail.com>
2022-06-20 20:42:58 -0400

    params-bar

--- setup-pipeline-bar.sh
+++ setup-pipeline-bar.sh
@@ -3,6 +3,6 @@ set -eux
 
 # This script automatically sets up the self-updating demo pipeline so you can begin experimenting
 fly --target=concourse login --concourse-url=http://localhost:8080 --username=test --password=test
-fly --target=concourse set-pipeline --pipeline=pipeline-bar --config=pipeline-bar.yml --load-vars-from params-bar.yml 
-fly --target=concourse unpause-pipeline --pipeline=pipeline-bar
+fly --target=concourse set-pipeline --pipeline=config-pipeline-bar --config=config-pipeline-bar.yml --load-vars-from params-bar.yml 
+fly --target=concourse unpause-pipeline --pipeline=config-pipeline-bar
 
--- setup-pipeline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eux
-
-# This script automatically sets up the self-updating demo pipeline so you can begin experimenting
-fly --target=concourse login --concourse-url=http://localhost:8080 --username=test --password=test
-fly --target=concourse set-pipeline --pipeline=hello --config=pipeline.yml --load-vars-from params.yml --var "params_file=params.yml"
-fly --target=concourse unpause-pipeline --pipeline=hello
-
